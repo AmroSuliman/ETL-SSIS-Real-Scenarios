fbf03368cbe1f592669507cca42cd7479f799a38
AMR SOLIMAN <129811361+AmroSuliman@users.noreply.github.com>
2025-10-05 15:01:06 +0300

    Add files via upload

---
+++ UseCase_19/19 Import files from multiple locations to SQL Server in SSIS/01_CreateTableScript.sql
@@ -0,0 +1,16 @@
+CREATE TABLE [dbo].[sample_data](
+	[id] [varchar](50) NULL,
+	[first_name] [varchar](50) NULL,
+	[last_name] [varchar](50) NULL,
+	[email] [varchar](50) NULL,
+	[gender] [varchar](50) NULL,
+	[Currency] [varchar](50) NULL
+) ON [PRIMARY]
+GO
+
+@[User::DestDirectory]+"\\"+RIGHT( @[User::FilePath] , FINDSTRING(REVERSE( @[User::FilePath] ) , "\\", 1) - 1)
+
+
+select 'C:\Data to load'
+union
+select 'D:\data to load'

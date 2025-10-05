c185b2ec05705d0b5f61eeba4ca20974918df663
AMR SOLIMAN <129811361+AmroSuliman@users.noreply.github.com>
2025-10-05 15:14:10 +0300

    Add files via upload
    Add organized SSIS scenario folders

---
+++ UseCase_43/43 Foreach Loop Container in SSIS/CreateTable.sql
@@ -0,0 +1,15 @@
+CREATE TABLE FileInfo(FileId int Identity, [FileName] varchar(max), RecordCount int, Dated datetime)
+go
+CREATE TABLE [dbo].[Emp](
+	[id] [varchar](50) NULL,
+	[first_name] [varchar](50) NULL,
+	[last_name] [varchar](50) NULL,
+	[Department] [varchar](50) NULL,
+	[gender] [varchar](50) NULL,
+	[FileId] [int] NULL
+) ON [PRIMARY]
+
+
+select * from Emp
+select * from FileInfo
+ select FileId,Count(*) from Emp group by FileId order by fileid asc

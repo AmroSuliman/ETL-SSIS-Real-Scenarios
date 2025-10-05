492cb446f44f77947437a3f53fe0ff923dd27c2b
AMR SOLIMAN <129811361+AmroSuliman@users.noreply.github.com>
2025-10-05 15:03:55 +0300

    Add files via upload
    Add organized SSIS scenario folders

---
+++ UseCase_26/26 From one CSV Import few columns to one destination and few to another in SSIS/01_CreateTable.sql
@@ -0,0 +1,6 @@
+create table Person(Id int, first_name varchar(50), last_name varchar(50), email varchar(100), gender varchar(50))
+create table Movie(Id int, Movie_Genres varchar(100), Movie_Title varchar(100))
+
+select * from Person
+
+select * from Movie

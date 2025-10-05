8f8096e30cf570a81ffc335fbc431e9814c2887d
AMR SOLIMAN <129811361+AmroSuliman@users.noreply.github.com>
2025-10-05 14:52:21 +0300

    Add files via upload
    Add organized SSIS scenario folders

---
+++ UseCase_04/01_CreateTable.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [dbo].[Emails04](
+	[Id] [int] NULL,
+	[First_name] [varchar](50) NULL,
+	[Last_name] [varchar](50) NULL,
+	[Email] [varchar](50) NULL,
+	[Gender] [char](1) NULL
+) ON [PRIMARY]
+
+SELECT * FROM [dbo].[Emails04]
+SELECT * FROM [dbo].[SSIS_BAD_ROWS_04]
